e2eea4c447ee07113447f4f3f0c84d7e1b7e655a
Yukio <elsie.atieno@student.moringaschool.com>
2023-12-29 23:41:56 +0300

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+import Companies from '@/components/shared/Companies'
+import Content from '@/components/shared/Content'
+import Featured from '@/components/shared/Featured'
+import Footer from '@/components/shared/Footer'
+import Header from '@/components/shared/Header'
+import HowItWorks from '@/components/shared/HowItWorks'
+import Statistics from '@/components/shared/Statistics'
+import Testimonials from '@/components/shared/Testimonials'
+
 
 export default function Home() {
   return (
     <>
-      <h1>Testing</h1>
+      <Header />
+      <Featured />
+      <Content />
+      <Statistics />
+      <HowItWorks />
+      <Companies />
+      <Testimonials />
+      <Footer />
     </>
   )
 }
